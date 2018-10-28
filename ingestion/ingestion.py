704e7aa608482e46a5c570d7dd686e86a4251e96
Erik Grueter <egrueter@jobcase.com>
2018-10-28 14:41:20 -0400

    Remove python import hack and add utils.py to dockerfile

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -4,26 +4,8 @@ import json
 import os
 import time
 import datetime
-import sys # imported this..
-
-# from utils import parse_log, is_get_request
-
-## <TODO: Fix this module import error!> ##
-# I know this is bad, sorry ;(
-def parse_log(msg):
-    # Retrieves relevant information from GET request
-    decomposed_message = msg.split(" ")
-    source = decomposed_message[0]
-    status = decomposed_message[-2]
-    time = decomposed_message[3][1:]
-    day = datetime.datetime.strptime(time, "%d/%b/%Y:%X").date()
-    return day, status, source
-
-def is_get_request(msg):
-    # Determines is msg is a GET request
-    decomposed_message = msg.split(" ")
-    return len(decomposed_message) >= 6 and decomposed_message[5] == "\"GET"
-## </TODO> ##
+import sys # imported this
+from utils import parse_log, is_get_request # add utils.py to Docker file
 
 #Connect to RabbitMQ
 credentials = pika.PlainCredentials(os.environ['RABBITMQ_DEFAULT_USER'], os.environ['RABBITMQ_DEFAULT_PASS'])
@@ -52,7 +34,7 @@ while True:
         if not msg: 
             break
 
-        #If message is GET request, ingest it into the queue
+        # If message is GET request, ingest it into the queue
         if is_get_request(msg):
             # Parse GET request for relevant information
             day, status, source = parse_log(msg)
